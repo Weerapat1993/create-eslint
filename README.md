d5e86adf7947b4b955a8040e32cb3cefeaf1a00e
Weerapat-Dotography <weerapat.t@dotography.com>
2018-01-22 10:38:08 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Create Eslint
+
+### How to Install
+
+```sh
+git clone https://github.com/Weerapat1993/create-eslint.git
+cd create-eslint
+npm install -g
+```
+
+### Usage
+
+```sh
+create-eslint [eslint-config-name]
+```
+
+### Eslint Config Name
+
+* react-app
+* react-native
+* airbnb
---
+++ eslint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#The following line will print no of argument provided to script
+USAGE="npm run eslint <eslint_config_name>"
+red=`tput setaf 1`;
+green=`tput setaf 2`;
+reset=`tput sgr0`;
+
+if [ "$#" -lt "1" ]
+then
+  echo " ";
+  echo "${red} error: missing required argument \`eslint_config_name\`";
+  echo " ";
+  echo "${green} --- Helper ---";
+  echo " usage : ${USAGE}${reset}";
+  echo " ";
+else
+  npm i -D babel-eslint eslint eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-config-$1
+
+  echo "${green}please add config in file package.json${reset}"
+  echo " ";
+  echo "  \"eslintConfig\": {"
+  echo "    \"extends\": \"$1\""
+  echo "  }"
+  echo " ";
+fi
